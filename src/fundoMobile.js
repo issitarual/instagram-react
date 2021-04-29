a1419cf457266f91ca8cde5cc051333511489dad
Laura Tissi <laura.tracierra@gmail.com>
2021-04-29 20:20:00 -0300

    html no js, fundo mobile com react

---
+++ src/fundoMobile.js
@@ -0,0 +1,12 @@
+export default function FundoMobile (){
+    const ionIconName = [
+        "home",
+        "search-outline",
+        "add-circle-outline",
+        "heart-outline",
+        "person-outline"];
+    return (
+    <div class="fundo-mobile">
+        {ionIconName.map(name => <ion-icon name={name}></ion-icon>)}
+    </div>);
+}
